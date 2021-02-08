ed0da86d6215227490b576e36c9e149e92b56273
arju10 <arjuarju71@gmail.com>
2021-02-08 16:04:30 +0600

    latest

--- javascript/app.js
+++ javascript/app.js
@@ -1,136 +1,47 @@
-const search = document.getElementById("search");
-const submit = document.getElementById("submit");
-const random = document.getElementById("random");
-const mealsEl = document.getElementById("meals");
-const resultHeading = document.getElementsByClassName(
-  "result-heading"
-);
-const single_mealEl = document.getElementById(
-  "single-meal"
-);
-
-//SearchMeal from API
-function searchMeal(e) {
-  e.preventDefault();
-
-  // Clear single Meal
-  single_mealEl.innerHTML = "";
-
-  //get search Term
-  const term = search.value;
-
-  //Check for empty
-  if (term.trim()) {
-    fetch(
-      `https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        resultHeading.innerHTML = `<h2>Search Result For ${term} : </h2>`;
-
-        if (data.meals === null) {
-          resultHeading.innerHTML = `<h2> There are No Search results for ${term}</h2>`;
-        } else {
-            mealsEl.innerHTML = data.meals
-            .map(
-              (meal) => `
-                 <div class="meal">
-                 <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-                 <div class="meal-info" data-mealID="${meal.idMeal}">
-                    <h3>${meal.strMeal}</h3>
-                 </div>
-                 </div>
-                `
-            )
-            .join("");
-        }
-      });
-
-    //Clear Search Term
-    search.value = "";
-  } else {
-    alert("please enter a search value");
-  }
-}
-
-//Fetch Meal By Id
-
-function getMealById(mealID) {
-  fetch(
-    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      const meal = data.meals[0];
-      addMealToDOM(meal);
-    });
-}
-
-//fetch Meal 
-function randomMeal(){
-    //Clear Meals and Heading
-    mealsEl.innerHTML='';
-    resultHeading.innerHTML='';
-
-    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-    .then(res => res.json())
-    .then(data => {
-        const meal = data.meals[0];
-        addMealToDOM(meal);
-    })
+const searchMeals = () =>{
+  const searchText = document.getElementById('search-field').value;
+  // console.log("clicked");
+  // console.log(searchText);
+  const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+  // console.log(url);
+  fetch(url)
+  .then(res => res.json())
+  .then(data =>displayMeals(data.categories))
 
 }
 
-//Add meal to DOM
+//display Meals
 
-function addMealToDOM(meal) {
-  const ingredients = [];
-  for (let i = 1; i <= 20; i++) {
-    if (meal[`strIngredient${i}`]) {
-      ingredients.push(
-        `${meal[`strIngredient${i}`]} - ${
-          meal[`strMeasure${i}`]
-        }`
-      );
-    }else{
-        break;
-    }
-  }
-
-  single_mealEl.innerHTML = `
-  <div class="single-meal">
-  <h1>${meal.strMeal}</h1>
-  <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
-  <div class="single-meal-info">
-  ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
-  ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
-  </div>
-  <div class="main">
-  <p>${meal.strInstructions}</p>
-  <h2>Ingredients</h2>
-  <ul>
-  ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
-  </ul>
-  </div>
-  </div>
-  `
-}
-
-//Event Listerner
-submit.addEventListener("submit", searchMeal);
-random.addEventListener('click',randomMeal);
-mealsEl.addEventListener("click", (e) => {
-  const mealInfo = e.path.find((item) => {
-    if (item.classList) {
-      return item.classList.contains("meal-info");
-    } else {
-      return false;
-    }
+const displayMeals = meals =>{
+  const mealContainer = document.getElementById('meal-container');
+  meals.forEach(meals => {
+     const mealDiv = document.createElement('div');
+     mealDiv.className = 'meals flex'
+     mealDiv.innerHTML =` 
+     <div class="single-meal">
+      <img src = ${meals.strCategoryThumb} onclick ='displayIngredient()'>
+ <h3>${meals.strCategory}</h3>
+ </div>`
+      mealContainer.appendChild(mealDiv);
   });
-  if (mealInfo) {
-    const mealID = mealInfo.getAttribute(
-      "data-mealid"
-    );
-    getMealById(mealID);
-  }
-});
+}
+     
+
+
+
+// After Clicking On Images
+
+const displayIngredient = ingredient =>{
+  const displayIngredient  = document.getElementById('displayIngredient');
+  ingredient.forEach(ingredient =>{
+      const ingredientDiv = document.createElement('div');
+      ingredientDiv.className = 'displayIngredient'
+      ingredientDiv.innerHTML= `
+          <img src =${ingredient.strCategoryThumb}>
+          <h3>${ingredient.strCategory}</h3>
+          
+          `
+          displayIngredient.appendChild(ingredientDiv);
+
+  })
+}
